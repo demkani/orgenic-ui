aeb836a70f7435ad4aaa8894163a5411b0bbea4e
Martin Kaniut <mk@derwirkstoff.de>
2019-09-28 00:22:14 +0200

    feat(og-radio-button): refresh radio-button

--- src/components/og-radio-button/og-radio-button.tsx
+++ src/components/og-radio-button/og-radio-button.tsx
@@ -4,15 +4,17 @@
  * See LICENSE file at https://github.com/orgenic/orgenic-ui/blob/master/LICENSE
  **/
 
-import { h, Component, Prop, Event, EventEmitter, Host } from '@stencil/core';
+import { h, Component, Prop, Event, EventEmitter, Host, State } from '@stencil/core';
 
 @Component({
   tag: 'og-radio-button',
   styleUrl: 'og-radio-button.scss',
-  // shadow dom off since we can't create button groups if each radio button is in it's own shadow dom
-  shadow: false
+  shadow: true
 })
 export class OgRadioButton {
+
+  @State() isFocused: boolean;
+
   /**
    * The name of the radio button. All radio buttons with the same name belong to one group.
    */
@@ -43,6 +45,9 @@ export class OgRadioButton {
   @Prop({ reflectToAttr: true })
   public disabled: boolean;
 
+  /** Moves the label to the other side of the radio control */
+  @Prop() opposite: boolean;
+
   /**
    * Determines, whether the control is disabled from the parent group
    */
@@ -52,30 +57,72 @@ export class OgRadioButton {
   @Event()
   public changed: EventEmitter<MouseEvent>;
 
-  private internalId = Math.random().toString(18).substring(2, 8) + Math.random().toString(18).substring(2, 8);
+  private generateUid() {
+    return (
+      Math.random()
+        .toString(36)
+        .substring(2, 15) +
+      Math.random()
+        .toString(36)
+        .substring(2, 15)
+    );
+  }
 
-  public eventInputChanged(event) {
-    this.changed.emit(event.target.value);
+  private uniqueId = this.generateUid();
+
+  public handleChange(e) {
+    if (!this.disabled) {
+      this.checked = e.target.checked;
+      this.changed.emit(e.target.checked);
+    }
+  }
+
+  private handleFocus(focus: boolean) {
+    if (!this.disabled) {
+      this.isFocused = focus;
+    }
   }
 
   public render(): HTMLElement {
     return (
-      <Host class={{
-        'og-radio-button--checked' : this.checked,
-        'og-radio-button--disabled' : this.disabled || this.groupDisabled,
-      }}>
-        <input
-          type="radio"
-          id={ this.internalId }
-          name={ this.name }
-          checked={ this.checked }
-          disabled={ this.disabled || this.groupDisabled }
-          class="og-radio-button__input"
-          onChange={ (e) => this.eventInputChanged(e) }/>
+      <Host
+        class={{
+          'is-checked': this.checked,
+          'is-focused': this.isFocused,
+          'is-disabled': this.disabled || this.groupDisabled,
+        }}
+      >
         <label
-          class="og-radio-button__label"
-          htmlFor={ this.internalId }>
-          { this.label && <span class="og-radio-button__label__content">{ this.label }</span> }
+          class="og-radio-button"
+          htmlFor={this.uniqueId}
+        >
+          <input
+            class="og-radio-button__input"
+            type="radio"
+            id={this.uniqueId}
+            checked={this.checked}
+            disabled={this.disabled || this.groupDisabled}
+            onChange={(e) => this.handleChange(e)}
+            onFocus={() => this.handleFocus(true)}
+            onBlur={() => this.handleFocus(false)}
+          />
+          {
+            !!this.label &&
+            !!this.opposite &&
+            <span class="og-radio-button__label">
+              {this.label}
+            </span>
+          }
+          <span class="og-radio-button__control">
+              <span class="og-radio-button__box__indicator"></span>
+          </span>
+          {
+            !!this.label &&
+            !this.opposite &&
+            <span class="og-radio-button__label">
+              {this.label}
+            </span>
+          }
         </label>
       </Host>
     );
