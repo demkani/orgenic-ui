1ab7ab7a4730c0cdce4ec84dfa3e65ca46544c39
Martin Kaniut <mk@derwirkstoff.de>
2019-11-23 00:18:57 +0100

    feat(og-expander): refresh expander and add transition

--- src/components/og-expander/og-expander.tsx
+++ src/components/og-expander/og-expander.tsx
@@ -4,7 +4,7 @@
  * See LICENSE file at https://github.com/orgenic/orgenic-ui/blob/master/LICENSE
  **/
 
-import { h, Component, Prop, Element, Method, Host } from '@stencil/core';
+import { h, Component, Prop, Element, Method, Host, Watch } from '@stencil/core';
 
 @Component({
   tag: 'og-expander',
@@ -13,7 +13,7 @@ import { h, Component, Prop, Element, Method, Host } from '@stencil/core';
 })
 export class OgExpander {
   @Element()
-  public el: HTMLElement;
+  public hostElement: HTMLElement;
 
   /**
    * The name for this expander
@@ -35,6 +35,23 @@ export class OgExpander {
   public expanded: boolean = false;
 
   /**
+   * Calculates and toggles the height of the Expander Body so it's possible
+   * to add CSS transitions on open and close.
+   */
+  @Watch('expanded')
+  public watchExpanded(value: boolean) {
+    const height = this.expanderContent.scrollHeight;
+    if (value) {
+      this.expanderBodyHeight = height + 'px';
+    } else {
+      this.expanderBodyHeight = '0';
+    }
+  }
+
+  private expanderBodyHeight = '0';
+  private expanderContent: HTMLElement;
+
+  /**
    * Use this method to toggle expanded state. Group property is respected when calling this method.
    */
   @Method()
@@ -42,7 +59,7 @@ export class OgExpander {
     if (this.group) {
       const elements = document.querySelectorAll('og-expander');
       elements.forEach((element: HTMLOgExpanderElement) => {
-        if (element.group === this.group && element !== this.el) {
+        if (element.group === this.group && element !== this.hostElement) {
           element.expanded = false;
         }
       });
@@ -52,38 +69,42 @@ export class OgExpander {
 
   public render(): HTMLElement {
     return (
-      <Host class={{ 'og-expander': true }}>
-        <div class="og-expander__header" onClick={() => { this.toggleExpandedState(); }}>
-          <span class="og-expander__title">{this.name}</span>
+      <Host>
+        <div class="og-expander">
+          <div class="og-expander__header" onClick={() => { this.toggleExpandedState(); }}>
+            <span class="og-expander__title">{this.name}</span>
 
-          <div class="og-expander__button">
-            <svg
-              class={
-                'og-expander__button__arrow' +
-                              (this.expanded
-                                ? ' og-expander__button__arrow--collapsed'
-                                : '')
-              }
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              viewBox="0 0 24 12"
-              preserveAspectRatio="none"
-            >
-              <polyline
-                class="og-expander__button__arrow__line"
-                points="0,0 12,12 24,0"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecaps="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <div class="og-expander__button">
+              <svg
+                class={
+                  'og-expander__button__arrow' +
+                                (this.expanded
+                                  ? ' og-expander__button__arrow--collapsed'
+                                  : '')
+                }
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 24 12"
+                preserveAspectRatio="none"
+              >
+                <polyline
+                  class="og-expander__button__arrow__line"
+                  points="0,0 12,12 24,0"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecaps="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
           </div>
-        </div>
-        <div class="og-expander__content" data-expanded={this.expanded}>
-          <slot></slot>
+          <div class="og-expander__body" style={{'height' : this.expanderBodyHeight }} >
+            <div class="og-expander__content" ref={(el) => this.expanderContent = el} >
+              <slot></slot>
+            </div>
+          </div>
         </div>
       </Host>
     );
